9d4dd9a50ca8489217e9241256122b19dea146a3
SeneSatka <senesatka@gmail.com>
2025-12-04 20:19:17 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-[#101010] w-dvw h-dvh flex items-center justify-center">
+    <div class="text-white text-6xl">Yakında!</div>
+  </div>
+</template>
+
+<style scoped></style>
